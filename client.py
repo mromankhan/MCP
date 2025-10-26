018e03f03af708d6dd3fce37ff82b38233d66e40
Muhammad Roman <mromankhan005@gmail.com>
2025-10-26 23:54:35 +0500

    Client for MCP

---
+++ client.py
@@ -0,0 +1,21 @@
+import json
+import requests
+
+url = "http://localhost:8000/mcp"
+
+headers = {
+    "Accept": "text/event-stream, application/json",
+}
+
+body = {
+    "jsonrpc": "2.0",
+    "method": "tools/list",
+    "id": 1,
+    "params": {}
+}
+
+response = requests.post(url, headers=headers, json=body)
+
+for line in response.iter_lines():
+    if line:
+        print(line)
--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def main():
-    print("Hello MCP-Server!")
+from re import search
+from mcp.server.fastmcp import FastMCP
+
+mcp = FastMCP(name="hello-MCP", stateless_http=True)
+
+@mcp.tool(name="online_researcher", description="Search the web for information")
+def search_online(query: str) -> str:
+    return f"Result of searching for {query}..."
 
 
-if __name__ == "__main__":
-    main()
+mcp_app = mcp.streamable_http_app()
+
