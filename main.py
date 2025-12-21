bb762ce6e9fed232eb57702721b4020023ac9b65
Muhammad Roman <mromankhan005@gmail.com>
2025-12-21 23:43:28 +0500

    weather tool removed

--- main.py
+++ main.py
@@ -2,11 +2,6 @@ from mcp.server.fastmcp import FastMCP
 
 mcp = FastMCP(name="hello-MCP", stateless_http=True)
 
-@mcp.tool(name="check_weather", description="Check the weather in a given location")
-def search_online(city: str) -> str:
-    print(f"Checking weather for {city}...")
-    return f"weather in {city} is sunny..."
-
 @mcp.tool(name="find_repo", description="Find github repo with given name")
 def find_repo(name: str) -> str:
     print(f"Finding repo with name {name}...")
